31e74d4c2a7685e498bfe58b0752b9775c46cb12
hhyxx <qwerasdf85246@gmail.com>
2021-08-09 21:51:41 +0800

    add

---
+++ ASP.NET/code template/template/数据库/command_delete.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebSite8{
+    public partial class command_delete : System.Web.UI.Page{
+        protected void Button1_Click(object sender, EventArgs e){
+            int intDeleteCount;
+            string sqlconnstr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(sqlconnstr);
+            //建立Command对象
+            SqlCommand sqlcommand = new SqlCommand();
+            //给Command对象的Connection和CommandText属性赋值
+            sqlcommand.Connection = sqlconn;
+            sqlcommand.CommandText = "delete from student where no=@no";
+            sqlcommand.Parameters.AddWithValue("@no", TextBox1.Text);
+            try{
+                sqlconn.Open();
+                // 返回的是删除的记录个数
+                intDeleteCount = sqlcommand.ExecuteNonQuery();
+                if (intDeleteCount > 0)
+                    Label1.Text = "成功删除记录";
+                else
+                    Label1.Text = "该记录不存在";
+            }
+            catch (Exception ex){
+                Label1.Text = "错误原因：" + ex.Message;
+            }
+            finally{
+                sqlcommand = null;
+                sqlconn.Close();
+                sqlconn = null;
+            }
+        }
+    }
+}
---
+++ ASP.NET/code template/template/数据库/建立数据库连接.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//引用数据库访问名称空间
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace WebSite8
+{
+    public partial class connection : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //从web.config配置文件取出数据库连接串
+            string sqlconnstr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            //建立数据库连接对象
+            SqlConnection sqlconn = new SqlConnection(sqlconnstr);
+            //打开连接对象
+            sqlconn.Open();
+            Label1.Text = "成功建立SQL Server 2012数据库连接";
+            //关闭连接对象
+            sqlconn.Close();
+            sqlconn = null;
+        }
+
+    }
+}
